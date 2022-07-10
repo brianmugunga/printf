54eaea1b837e6a5b016df9ad8561fef92eba8726
liambilahezron <liambilahezron1990@gmail.com>
2022-07-10 16:09:38 +0300

    Team printf main.h file

--- main.h
+++ main.h
@@ -1,7 +1,9 @@
 #ifndef HEADER_MAIN
 #define HEADER_MAIN
 #include <stdarg.h>
-
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
 int _printf(const char *format, ...);
 char *convert_to(char representation[], unsigned int num, int base);
 int function_manager(char c, va_list arg);
@@ -15,6 +17,8 @@ int print_rot13(va_list arg);
 int print_rev(va_list arg);
 int print_base16_upper_lower(va_list arg, char *representation);
 int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
 
 //the below to me are what i think should be in our header file.
 
