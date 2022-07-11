f38833acf6ac4386680886f647e93ac5dd51be28
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-07-11 19:05:27 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,7 +2,11 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
-
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
 typedef struct print
 {
 	char *t;
