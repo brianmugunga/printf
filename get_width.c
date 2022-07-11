eb65073540acd6691d913d6d61fedee5ecfc0130
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-07-11 16:30:44 +0300

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -25,4 +25,12 @@ int get_width(const char *format, int *i, va_list list)
 			curr_i++;
 			width = va_arg(list, int);
 			break;
+	}
+		else
+			break;
+	}
 
+	*i = curr_i - 1;
+
+	return (width);
+}
