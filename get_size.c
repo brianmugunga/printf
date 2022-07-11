b69e047cd9795a02ac661de0c0409bb2d6c395f2
liambilahezron <liambilahezron1990@gmail.com>
2022-07-11 13:48:32 +0300

    get_size team printf

--- get_size.c
+++ get_size.c
@@ -1,23 +1,34 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
+ * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
+ * @i: take a parameter.
+ * Return: Flags:
  */
-int get_size(const char *format, int *i)
+int get_flags(const char *format, int *i)
 {
-	int curr_i = *i + 1;
-	int size = 0;
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	if (format[curr_i] == 'l')
-		size = S_LONG;
-	else if (format[curr_i] == 'h')
-		size = S_SHORT;
-  
-  
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
 
-	return (size);
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (flags);
 }
