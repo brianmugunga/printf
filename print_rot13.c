9b4676617e1c337c1873b3fdeb31dc364b3c71f4
liambilahezron <liambilahezron1990@gmail.com>
2022-07-11 18:58:59 +0300

    Team printf project

--- print_rot13.c
+++ print_rot13.c
@@ -18,4 +18,23 @@ int print_R(va_list R)
 	str = va_arg(R, char *);
 	if (str == NULL)
 		str = "(ahyy)";
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[i])
+			{
+				_putchar(out[j]);
+				count++;
+				break;
+			}
+		}
+		if (!in[j])
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
 
